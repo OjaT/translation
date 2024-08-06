19018878a35bc42a3eb7e7bdfdf3b75cd5822d4d
Taavi Oja <oja.taavi@gmail.com>
2024-08-06 11:19:13 +0300

    Array of words to be translated

--- translation.php
+++ translation.php
@@ -6,3 +6,15 @@
  */
 
 require( plugin_dir_path( __FILE__ ) . '/acf/fields.php');
+
+function getStrings() {
+    $a = array();
+    if( have_rows('translation_string', 'option') ):
+        while( have_rows('translation_string', 'option') ) : the_row();   
+            $s = get_sub_field('text');
+            $s = esc_attr(trim($s));
+            array_push($a, $s);
+        endwhile;
+    endif;
+    return $a;
+}
